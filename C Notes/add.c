df0149f8cfea43798ca23464b5f14a50d66aafcb
sammedtech <127331049+sammedtech@users.noreply.github.com>
2023-03-10 17:23:47 +0530

    Delete add.c

--- C Notes/add.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> // header file
-
-int main(){ // entry point
-    int first_val; // variable declaration
-    int second_val;
-    first_val = 10; // variable definition
-    second_val = 20;
-    printf("The total sum : %d\n", first_val+second_val);
-}
