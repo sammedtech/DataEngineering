6428c18aeacc498f5fa286ec2ebd6da3460246b3
sammedtech <127331049+sammedtech@users.noreply.github.com>
2023-03-10 17:22:47 +0530

    session2 codes

---
+++ C Notes/Session2_09MAR2023/addition.h
@@ -0,0 +1,5 @@
+#include <stdio.h> // library/module
+// datatype function_name(arguments){} --> syntax for function
+void addition(int a, int b){
+    printf("%d\n", a+b); // printf is under stdio.h file
+}
