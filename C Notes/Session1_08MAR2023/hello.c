d191d0c3d75edd5d88dd6558763a71d59a9472b8
sammedtech <127331049+sammedtech@users.noreply.github.com>
2023-03-10 17:24:27 +0530

    session1 programs

---
+++ C Notes/Session1_08MAR2023/hello.c
@@ -0,0 +1,5 @@
+#include <stdio.h> // header file
+
+int main(){ // execution point for C
+    printf("Hello Akash"); // printf --> To print text on your screeen
+} // syntax
---
+++ C Notes/Session1_08MAR2023/whats_your_name.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // header file
+
+int main(){ // entry point
+    int age; // variable declaration
+    printf("Hello, whats your age? : \n"); // ; -> end of line character
+    scanf("%d", &age); // scanf --> to accept input from user
+    printf("Your Age : %d \n", age); 
+}
