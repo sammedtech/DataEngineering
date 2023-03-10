0782873ed8eba991723c56fb732e3fd33785a7e9
sammedtech <127331049+sammedtech@users.noreply.github.com>
2023-03-10 17:22:19 +0530

    Delete addition.h

--- C Notes/addition.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h> // library/module
-// datatype function_name(arguments){} --> syntax for function
-void addition(int a, int b){
-    printf("%d\n", a+b); // printf is under stdio.h file
-}
